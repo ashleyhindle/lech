744af3edcecde2b0a5f20b4c5f87412e63b6d9e8
Ashley Hindle <hey@ashleyhindle.com>
2025-01-28 13:47:44 +0000

    feat: adds console command to shorten URLs through artisan

---
+++ app/Console/Commands/ShortenUrl.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Actions\Encode;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Validator;
+
+class ShortenUrl extends Command
+{
+    protected $signature = 'app:shorten {url : The URL to shorten}';
+
+    protected $description = 'Shortens a long URL for you';
+
+    public function handle(): void
+    {
+        $args = $this->validateArguments();
+        $encodedUrl = (new Encode)($args['url']);
+        $this->info($encodedUrl);
+    }
+
+    protected function validateArguments(): ?array
+    {
+        $validator = Validator::make($this->arguments(), [
+            'url' => 'required|url',
+        ]);
+
+        if ($validator->fails()) {
+            $this->error('Oh no, your URL is dodgy. Try again with a valid URL.');
+            collect($validator->errors()->all())->each(fn ($error) => $this->warn($error));
+            exit(1);
+        }
+
+        return $validator->validated();
+    }
+}
