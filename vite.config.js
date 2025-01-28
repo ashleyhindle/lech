d96dcfda87183a01f4ab0ab6cd089e1c3ae21d61
Ashley Hindle <hey@ashleyhindle.com>
2025-01-28 14:55:08 +0000

    fix: fixes npm install legacy peer deps issue

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,5 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-    ],
-    server: {
-        cors: {
-            origin: [
-                'https://lech.test',
-                'https://lech.test:5173',
-                'https://lech.test:5174',
-            ],
-        },
-    },
+    ]
 });
